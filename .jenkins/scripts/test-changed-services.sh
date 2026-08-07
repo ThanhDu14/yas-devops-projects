83395eea32d388cf27b8e6e82749e68768e963d3
Thanhdu14 <thanhdunguyen561@gmail.com>
2026-08-07 13:52:20 +0700

    fix(ci): pass elasticsearch.url=host.docker.internal in CI script for DinD

--- .jenkins/scripts/test-changed-services.sh
+++ .jenkins/scripts/test-changed-services.sh
@@ -14,7 +14,7 @@ SERVICE_LIST=$(echo "${CHANGED_SERVICES}" | tr ' ' ',')
 
 if [ "${RUN_INTEGRATION_TESTS}" = "true" ]; then
     echo "Running Unit + Integration Tests (mvn verify) in parallel for: ${SERVICE_LIST}"
-    mvn -B -pl "${SERVICE_LIST}" -am verify -T 1C
+    mvn -B -pl "${SERVICE_LIST}" -am verify -T 1C -Delasticsearch.url=host.docker.internal
 else
     echo "Running Unit Tests (mvn test) in parallel for: ${SERVICE_LIST}"
     mvn -B -pl "${SERVICE_LIST}" -am test -T 1C -DskipITs

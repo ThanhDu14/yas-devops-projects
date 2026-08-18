bbef46097bb0a37e610c6f1f09e94fc84fe22285
Thanhdu14 <thanhdunguyen561@gmail.com>
2026-08-18 10:02:03 +0700

    chore: stop tracking terraform directory

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "load_balancer_ip" {
-  description = "IP Public cua Load Balancer"
-  value       = google_compute_global_forwarding_rule.yas_forwarding_rule.ip_address
-}
-
-output "artifact_registry_url" {
-  description = "URL kho chua Docker Image tren GCP"
-  value       = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.yas_repo.repository_id}"
-}
